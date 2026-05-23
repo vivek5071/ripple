2b7ae9fb354c9ee827570ca48f0b840748dd4f74
Vivek <v58415071@gmail.com>
2026-05-23 20:03:53 +0500

    fix: remove source-map from ncc build — sourcemap-register.js not committed

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 1445:
@@ -41484,4 +41484,3 @@ module.exports = parseParams
 /******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
